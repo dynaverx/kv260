c1b49701987ac60ea3dcf3c2dd5b12524ae1f3a5
Fatih Geridönmez <fgerid@gmail.com>
2023-04-22 02:29:59 +0300

    Update set_DP-1.sh

--- set_DP-1.sh
+++ set_DP-1.sh
@@ -2,5 +2,12 @@
 xrandr --newmode "1024x600_60.00" 49.00 1024 1029 1042 1312 600 602 605 622 -HSync +VSync
 xrandr --addmode DP-1 "1024x600_60.00"
 xrandr --output DP-1 --mode "1024x600_60.00"
+
+ifconfig eth0 ---.---.--.--- netmask 255.255.255.0
+
+sleep 1
+xset dpms 0 0 0 && xset -dpms && xset s off && xset s noblank
+
+
 cd /home
 ./app
